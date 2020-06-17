6d519cffac5fc265443975bda71dabc0a0f3c232
Kenneth Rogers <kenneth.rogers2@navy.mil>
2020-06-17 14:50:16 -0400

    Refactored fireWeapon method.

--- TestedTrek/TestedTrek/src/StarTrek/Game.h
+++ TestedTrek/TestedTrek/src/StarTrek/Game.h
@@ -8,23 +8,32 @@
 namespace StarTrek {
 
 class Galaxy;
+class Klingon;
 
 typedef int(*Random)(void);
 
 
 class Game {
 private:
-	int e_;
-	int t_;
+    int m_energy;  // Amount of energy
+    int m_torpedoes;  // Number of torpedoes
 
 	static int rnd(int maximum) {
 		return generator() % maximum; 
 	}
 
+    void damageEnemy(Galaxy& galaxy,
+                     const std::string& weapon_type,
+                     int damage,
+                     int distance,
+                     Klingon* enemy);
+
 public:
     Game();
     void fireWeapon(Untouchables::WebGadget* wg);
     void fireWeapon(Galaxy& galaxy);
+    void firePhaser(Galaxy& galaxy);
+    void firePhotonTorpedo(Galaxy& galaxy);
     int energyRemaining(void);
     void torpedoes(int value);
     int torpedoes(void);
